a20ba65a400393dea01eaadb4d44368cf5b25687
ssaemo <jeontha@gmail.com>
2017-10-30 16:26:05 +0900

    complete go back to the full map

--- app/src/main/java/com/kjh/seoulapp/TourMainActivity.java
+++ app/src/main/java/com/kjh/seoulapp/TourMainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.util.ArrayMap;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -34,6 +35,10 @@ import com.google.firebase.database.ValueEventListener;
 import com.kjh.seoulapp.data.CulturalData;
 import com.kjh.seoulapp.data.SharedData;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static com.kjh.seoulapp.data.SharedData.CULTURAL_REF;
 import static com.kjh.seoulapp.data.SharedData.DATA_NAME;
 import static com.kjh.seoulapp.data.SharedData.POPUP_TYPE;
@@ -43,12 +48,17 @@ import static com.kjh.seoulapp.data.SharedData.regionIndex;
 public class TourMainActivity extends GoogleApiClientActivity
 		implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener
 {
-	private final long FINISH_INTERVAL_TIME = 2000;
-	private long backPressedTime = 0;
-	private boolean backPressedcheck = false;
-
+	enum MAP_TYPE { FULL, MID, EAST, WEST, SOUTH, NORTH}
+	final long FINISH_INTERVAL_TIME = 2000;
 	final String TAG = "TourMainActivity";
+
+	long backPressedTime;
 	FirebaseAuth auth;
+	MAP_TYPE nowMap;
+	ImageView fullmapView;
+	Map<MAP_TYPE, ImageView> mapMap;
+	List<Button> hiddenBtnList;
+	Map<MAP_TYPE, List<ImageButton>> mapRegion;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -57,6 +67,7 @@ public class TourMainActivity extends GoogleApiClientActivity
 		android.util.Log.d("TAG","MAX MEMORY : "+(Runtime.getRuntime().maxMemory() / (1024 * 1024)) + "MB");
 		android.util.Log.d("TAG","FREE MEMORY : "+(Runtime.getRuntime().freeMemory() / (1024 * 1024)) + "MB");
 		android.util.Log.d("TAG","ALLOCATION MEMORY : "+((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024)) + "MB");
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tour_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
@@ -70,10 +81,56 @@ public class TourMainActivity extends GoogleApiClientActivity
 		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 		navigationView.setNavigationItemSelectedListener(this);
 
-		auth = FirebaseAuth.getInstance();
-		regionIndex = -1;
+
+		/* init members */
 		progressBar = (ProgressBar) findViewById(R.id.progressBarMain);
 		hideProgressDialog();
+		regionIndex = 0;
+
+		backPressedTime = 0;
+		auth = FirebaseAuth.getInstance();
+		nowMap = MAP_TYPE.FULL;
+
+		fullmapView = (ImageView) findViewById(R.id.map_full);
+
+		mapMap = new ArrayMap<>();
+		mapMap.put(MAP_TYPE.MID, (ImageView) findViewById(R.id.map_1));
+		mapMap.put(MAP_TYPE.EAST, (ImageView) findViewById(R.id.map_2));
+		mapMap.put(MAP_TYPE.WEST, (ImageView) findViewById(R.id.map_3));
+		mapMap.put(MAP_TYPE.SOUTH, (ImageView) findViewById(R.id.map_4));
+		mapMap.put(MAP_TYPE.NORTH, (ImageView) findViewById(R.id.map_5));
+
+		hiddenBtnList = new ArrayList<>();
+		hiddenBtnList.add((Button) findViewById(R.id.map_mid_button));
+		hiddenBtnList.add((Button) findViewById(R.id.map_east_button));
+		hiddenBtnList.add((Button) findViewById(R.id.map_west_button));
+		hiddenBtnList.add((Button) findViewById(R.id.map_south_button));
+		hiddenBtnList.add((Button) findViewById(R.id.map_north_button));
+
+		mapRegion = new ArrayMap<>();
+		mapRegion.put(MAP_TYPE.MID, new ArrayList<ImageButton>());
+		mapRegion.put(MAP_TYPE.EAST, new ArrayList<ImageButton>());
+		mapRegion.put(MAP_TYPE.WEST, new ArrayList<ImageButton>());
+		mapRegion.put(MAP_TYPE.SOUTH, new ArrayList<ImageButton>());
+		mapRegion.put(MAP_TYPE.NORTH, new ArrayList<ImageButton>());
+
+		List<ImageButton> tempList;
+
+		tempList = mapRegion.get(MAP_TYPE.MID);
+		tempList.add((ImageButton) findViewById(R.id.icon_jongmyo));
+		tempList.add((ImageButton) findViewById(R.id.icon_indepen));
+		tempList.add((ImageButton) findViewById(R.id.icon_gyungbok));
+		tempList.add((ImageButton) findViewById(R.id.icon_changduck));
+		tempList.add((ImageButton) findViewById(R.id.icon_changgyung));
+		tempList.add((ImageButton) findViewById(R.id.icon_gyunghee));
+		tempList.add((ImageButton) findViewById(R.id.icon_ducksu));
+		tempList.add((ImageButton) findViewById(R.id.icon_busin));
+		tempList.add((ImageButton) findViewById(R.id.icon_dongdaemun));
+		tempList.add((ImageButton) findViewById(R.id.icon_namdaemun));
+		tempList.add((ImageButton) findViewById(R.id.icon_bukdaemun));
+
+		tempList = mapRegion.get(MAP_TYPE.SOUTH);
+		tempList.add((ImageButton) findViewById(R.id.icon_nakjungdae));
 
 		FirebaseUser user = auth.getCurrentUser();
 		if (user != null)
@@ -112,27 +169,139 @@ public class TourMainActivity extends GoogleApiClientActivity
 	@Override // button event: open drawer
 	public void onBackPressed()
 	{
-		long tempTime = System.currentTimeMillis();
-		long intervalTime = tempTime - backPressedTime;
-
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
 		if (drawer.isDrawerOpen(GravityCompat.START))
-		{
 			drawer.closeDrawer(GravityCompat.START);
-		} else
+		else
 		{
-			if(backPressedcheck) {
-			}
-			else{
-				if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
-					super.onBackPressed();
-				} else {
+			if(nowMap == MAP_TYPE.FULL) // 전체지도 상태일 경우
+			{
+				long tempTime = System.currentTimeMillis();
+				long intervalTime = tempTime - backPressedTime;
+
+				if (!(0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime)) // 최초 Back Pressed일 경우
+				{
 					backPressedTime = tempTime;
-					Toast.makeText(getApplicationContext(), "종료하시려면 한번 더 누르세요", Toast.LENGTH_SHORT).show();
+					Toast.makeText(TourMainActivity.this, "종료하시려면 한번 더 누르세요", Toast.LENGTH_SHORT).show();
+				}
+				else // 연속 Back Pressed일 경우
+				{
+					super.onBackPressed();
 				}
 			}
+			else // 구역지도 상태일 경우
+				goOutMap();
 		}
 	}
+
+	@Override
+	public void onClick(View v)
+	{
+		if (isProgress)
+			return;
+
+		int id = v.getId();
+
+		switch (id)
+		{
+			case R.id.cam_test:
+				Intent camIntent = new Intent(TourMainActivity.this, ARActivity.class);
+				startActivity(camIntent);
+				break;
+			case R.id.btn_next: startRegionActivity(1); break;
+			case R.id.icon_jongmyo: startRegionActivity(1); break;
+			case R.id.icon_nakjungdae: startRegionActivity(2); break;
+			case R.id.icon_indepen: startRegionActivity(3); break;
+			case R.id.icon_gyungbok: startRegionActivity(4); break;
+			case R.id.icon_changduck: startRegionActivity(5); break;
+			case R.id.icon_changgyung: startRegionActivity(6); break;
+			case R.id.icon_gyunghee: startRegionActivity(7); break;
+			case R.id.icon_ducksu: startRegionActivity(8); break;
+			case R.id.map_mid_button: goInMap(MAP_TYPE.MID); break;
+			case R.id.map_east_button: goInMap(MAP_TYPE.EAST); break;
+			case R.id.map_west_button: goInMap(MAP_TYPE.WEST); break;
+			case R.id.map_south_button: goInMap(MAP_TYPE.SOUTH); break;
+			case R.id.map_north_button: goInMap(MAP_TYPE.NORTH); break;
+		}
+	} // onClick()
+
+	void goInMap(MAP_TYPE map)
+	{
+		fullmapView.setVisibility(View.GONE);
+		for (Button btn : hiddenBtnList)
+			btn.setVisibility(View.GONE);
+		_updateMap(map, View.VISIBLE);
+		nowMap = map;
+	}
+
+	void goOutMap()
+	{
+		fullmapView.setVisibility(View.VISIBLE);
+		for (Button btn : hiddenBtnList)
+			btn.setVisibility(View.VISIBLE);
+		_updateMap(nowMap, View.GONE);
+		nowMap = MAP_TYPE.FULL;
+	}
+
+	void _updateMap(MAP_TYPE map, int visibility)
+	{
+		mapMap.get(map).setVisibility(visibility);
+		List<ImageButton> tempList = mapRegion.get(map);
+		for(ImageButton btnRegion : tempList)
+			btnRegion.setVisibility(visibility);
+	}
+
+	private void signOut()
+	{
+		// Firebase sign out
+		auth.signOut();
+
+		// Google sign out
+		Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>()
+		{
+			@Override
+			public void onResult(@NonNull Status status)
+			{
+				Intent intent = new Intent(TourMainActivity.this, SocialLoginActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+	} // signOut()
+
+	void startRegionActivity(int idx)
+	{
+		regionIndex = idx;
+
+		DatabaseReference ref = FirebaseDatabase.getInstance().getReference(CULTURAL_REF).child(""+regionIndex);
+		Log.v(TAG, ref.toString());
+
+		ValueEventListener listener = new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				hideProgressDialog();
+				CulturalData cultural = dataSnapshot.getValue(CulturalData.class);
+				Log.d(TAG, "Value is: " + cultural);
+
+				SharedData.cultural = cultural;
+				hideProgressDialog();
+
+				Intent intent = new Intent(TourMainActivity.this, TourRegionActivity.class);
+				startActivity(intent);
+			}
+
+			@Override
+			public void onCancelled(DatabaseError e) {
+				hideProgressDialog();
+				Log.w(TAG, "Failed to read value.", e.toException());
+				Toast.makeText(TourMainActivity.this, "데이터 가져오기 실패", Toast.LENGTH_SHORT).show();
+			}
+		};
+
+		showProgressDialog();
+		addListenerWithTimeout(this, ref, listener, DATA_NAME.CULTURAL);
+	} // startRegionActivity()
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
@@ -199,202 +368,10 @@ public class TourMainActivity extends GoogleApiClientActivity
 		return true;
 	} // onNavigationItemSelected()
 
-	@Override
-	public void onClick(View v)
-	{
-		if (isProgress)
-			return;
-
-		int id = v.getId();
-
-		ImageView map_full = (ImageView) findViewById(R.id.map_full);
-		ImageView map_1 = (ImageView) findViewById(R.id.map_1);
-		ImageView map_2 = (ImageView) findViewById(R.id.map_2);
-		ImageView map_3 = (ImageView) findViewById(R.id.map_3);
-		ImageView map_4 = (ImageView) findViewById(R.id.map_4);
-		ImageView map_5 = (ImageView) findViewById(R.id.map_5);
-		Button map_mid_button = (Button) findViewById(R.id.map_mid_button);
-		Button map_east_button = (Button) findViewById(R.id.map_east_button);
-		Button map_west_button = (Button) findViewById(R.id.map_west_button);
-		Button map_south_button = (Button) findViewById(R.id.map_south_button);
-		Button map_north_button = (Button) findViewById(R.id.map_north_button);
-		ImageButton icon_jongmyo = (ImageButton) findViewById(R.id.icon_jongmyo);
-		ImageButton icon_nakjungdae = (ImageButton) findViewById(R.id.icon_nakjungdae);
-		ImageButton icon_indepen = (ImageButton) findViewById(R.id.icon_indepen);
-		ImageButton icon_gyungbok = (ImageButton) findViewById(R.id.icon_gyungbok);
-		ImageButton icon_changduck = (ImageButton) findViewById(R.id.icon_changduck);
-		ImageButton icon_changgyung = (ImageButton) findViewById(R.id.icon_changgyung);
-		ImageButton icon_gyunghee = (ImageButton) findViewById(R.id.icon_gyunghee);
-		ImageButton icon_ducksu = (ImageButton) findViewById(R.id.icon_ducksu);
-		ImageButton icon_busin = (ImageButton) findViewById(R.id.icon_busin);
-		ImageButton icon_dongdaemun = (ImageButton) findViewById(R.id.icon_dongdaemun);
-		ImageButton icon_namdaemun = (ImageButton) findViewById(R.id.icon_namdaemun);
-		ImageButton icon_bukdaemun = (ImageButton) findViewById(R.id.icon_bukdaemun);
-
-		switch (id)
-		{
-			case R.id.cam_test:
-				Intent camIntent = new Intent(TourMainActivity.this, ARActivity.class);
-				startActivity(camIntent);
-				break;
-			case R.id.btn_next:
-				regionIndex = 1;
-				startRegionActivity();
-				break;
-			case R.id.icon_jongmyo:
-				regionIndex = 1;
-				startRegionActivity();
-				break;
-			case R.id.icon_nakjungdae:
-				regionIndex = 2;
-				startRegionActivity();
-				break;
-			case R.id.icon_indepen:
-				regionIndex = 3;
-				startRegionActivity();
-				break;
-			case R.id.icon_gyungbok:
-				regionIndex = 4;
-				startRegionActivity();
-				break;
-			case R.id.icon_changduck:
-				regionIndex = 5;
-				startRegionActivity();
-				break;
-			case R.id.icon_changgyung:
-				regionIndex = 6;
-				startRegionActivity();
-				break;
-			case R.id.icon_gyunghee:
-				regionIndex = 7;
-				startRegionActivity();
-				break;
-			case R.id.icon_ducksu:
-				regionIndex = 8;
-				startRegionActivity();
-				break;
-			case R.id.map_mid_button:
-				backPressedcheck=true;
-				map_full.setVisibility(View.GONE);
-				map_mid_button.setVisibility(View.GONE);
-				map_east_button.setVisibility(View.GONE);
-				map_west_button.setVisibility(View.GONE);
-				map_south_button.setVisibility(View.GONE);
-				map_north_button.setVisibility(View.GONE);
-				map_1.setVisibility(View.VISIBLE);
-				icon_jongmyo.setVisibility(View.VISIBLE);
-				icon_indepen.setVisibility(View.VISIBLE);
-				icon_gyungbok.setVisibility(View.VISIBLE);
-				icon_changduck.setVisibility(View.VISIBLE);
-				icon_changgyung.setVisibility(View.VISIBLE);
-				icon_gyunghee.setVisibility(View.VISIBLE);
-				icon_ducksu.setVisibility(View.VISIBLE);
-				icon_busin.setVisibility(View.VISIBLE);
-				icon_dongdaemun.setVisibility(View.VISIBLE);
-				icon_namdaemun.setVisibility(View.VISIBLE);
-				icon_bukdaemun.setVisibility(View.VISIBLE);
-				break;
-			case R.id.map_east_button:
-				backPressedcheck=true;
-				map_full.setVisibility(View.GONE);
-				map_mid_button.setVisibility(View.GONE);
-				map_east_button.setVisibility(View.GONE);
-				map_west_button.setVisibility(View.GONE);
-				map_south_button.setVisibility(View.GONE);
-				map_north_button.setVisibility(View.GONE);
-				map_2.setVisibility(View.VISIBLE);
-				break;
-			case R.id.map_west_button:
-				backPressedcheck=true;
-				map_full.setVisibility(View.GONE);
-				map_mid_button.setVisibility(View.GONE);
-				map_east_button.setVisibility(View.GONE);
-				map_west_button.setVisibility(View.GONE);
-				map_south_button.setVisibility(View.GONE);
-				map_north_button.setVisibility(View.GONE);
-				map_3.setVisibility(View.VISIBLE);
-				break;
-			case R.id.map_south_button:
-				backPressedcheck=true;
-				map_full.setVisibility(View.GONE);
-				map_mid_button.setVisibility(View.GONE);
-				map_east_button.setVisibility(View.GONE);
-				map_west_button.setVisibility(View.GONE);
-				map_south_button.setVisibility(View.GONE);
-				map_north_button.setVisibility(View.GONE);
-				map_4.setVisibility(View.VISIBLE);
-				icon_nakjungdae.setVisibility(View.VISIBLE);
-				break;
-			case R.id.map_north_button:
-				backPressedcheck=true;
-				map_full.setVisibility(View.GONE);
-				map_mid_button.setVisibility(View.GONE);
-				map_east_button.setVisibility(View.GONE);
-				map_west_button.setVisibility(View.GONE);
-				map_south_button.setVisibility(View.GONE);
-				map_north_button.setVisibility(View.GONE);
-				map_5.setVisibility(View.VISIBLE);
-				break;
-
-		}
-	} // onClick()
-
-	private void signOut()
-	{
-		// Firebase sign out
-		auth.signOut();
-
-		// Google sign out
-		Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>()
-		{
-			@Override
-			public void onResult(@NonNull Status status)
-			{
-				Intent intent = new Intent(TourMainActivity.this, SocialLoginActivity.class);
-				startActivity(intent);
-				finish();
-			}
-		});
-	} // signOut()
-
 	void popupActivity(POPUP_TYPE type)
 	{
 		Intent intent = new Intent(TourMainActivity.this, PopupActivity.class);
 		intent.putExtra("POPUP_TYPE", type);
 		startActivity(intent);
 	}
-
-	void startRegionActivity()
-	{
-		if (regionIndex == -1)
-			Log.d(TAG, "regionIndex is not initialized");
-
-		DatabaseReference ref = FirebaseDatabase.getInstance().getReference(CULTURAL_REF).child(""+regionIndex);
-		Log.v(TAG, ref.toString());
-
-		ValueEventListener listener = new ValueEventListener() {
-			@Override
-			public void onDataChange(DataSnapshot dataSnapshot) {
-				hideProgressDialog();
-				CulturalData cultural = dataSnapshot.getValue(CulturalData.class);
-				Log.d(TAG, "Value is: " + cultural);
-
-				SharedData.cultural = cultural;
-				hideProgressDialog();
-
-				Intent intent = new Intent(TourMainActivity.this, TourRegionActivity.class);
-				startActivity(intent);
-			}
-
-			@Override
-			public void onCancelled(DatabaseError e) {
-				hideProgressDialog();
-				Log.w(TAG, "Failed to read value.", e.toException());
-				Toast.makeText(TourMainActivity.this, "데이터 가져오기 실패", Toast.LENGTH_SHORT).show();
-			}
-		};
-
-		showProgressDialog();
-		addListenerWithTimeout(this, ref, listener, DATA_NAME.CULTURAL);
-	} // startRegionActivity()
 } // class
